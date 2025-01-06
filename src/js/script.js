c867c00781ccb9186e5486f93f543b81201b31c6
Henry-Elois <elois.henry@edu.univ-fcomte.fr>
2025-01-06 16:47:41 +0100

    carrousel

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,34 @@ document.querySelector(".menu-back").addEventListener("click", function () {
   document.getElementById("mainNav").setAttribute("aria-hidden", "true");
 });
 
+// Script pour le carrousel
+const carousel = document.querySelector(".carousel");
+const items = document.querySelectorAll(".carousel-item");
+const prevButton = document.querySelector(".carousel-button.prev");
+const nextButton = document.querySelector(".carousel-button.next");
+
+let currentIndex = 1;
+
+function updateCarousel() {
+  items.forEach((item, index) => {
+    item.classList.remove("active");
+    if (index === currentIndex) {
+      item.classList.add("active");
+    }
+  });
+}
+
+function showNextItem() {
+  currentIndex = (currentIndex + 1) % items.length;
+  updateCarousel();
+}
+
+function showPrevItem() {
+  currentIndex = (currentIndex - 1 + items.length) % items.length;
+  updateCarousel();
+}
+
+nextButton.addEventListener("click", showNextItem);
+prevButton.addEventListener("click", showPrevItem);
+
+setInterval(showNextItem, 5000);
