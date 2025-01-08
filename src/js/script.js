434c066d89d45e5e77d9c97647dec7f869edc2e3
Henry-Elois <elois.henry@edu.univ-fcomte.fr>
2025-01-08 08:18:13 +0100

    lightbox

--- src/js/script.js
+++ src/js/script.js
@@ -31,3 +31,27 @@ if (prevButton) {
   });
 }
 
+const lightBox = document.querySelector("#lightbox");
+const lightBoxImg = lightBox.querySelector("img");
+
+document.body.addEventListener("click", (evt) => {
+  console.log(evt.target);
+
+  if (evt.target.matches("[data-full-image]")) {
+    lightBoxImg.src = evt.target.dataset.fullImage;
+    lightBox.showModal();
+  }
+});
+
+lightBox.addEventListener("click", (evt) => {
+  lightBox.classList.add("sortie");
+
+  lightBox.addEventListener(
+    "animationend",
+    () => {
+      lightBox.classList.remove("sortie");
+      lightBox.close();
+    },
+    { once: true }
+  );
+}); 
